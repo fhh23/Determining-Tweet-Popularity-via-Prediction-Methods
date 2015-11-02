04dd35d80e8a1261b1fa77c7fa18966d9c5b64a9
Bonnie R <breiff13@gmail.com>
2015-11-02 00:22:29 -0500

    Modified the my_streaming_callback function to the print the output to a file. In the process of getting ALL tweet attributes to print the file. Subsequent step is to print all data in CSV file format.

--- getTwitter.php
+++ getTwitter.php
@@ -13,7 +13,7 @@ require 'tmhUtilities.php';
 // ------------------------------------------
 
 $params = array();
-$params['delimited'] = "length"; // Provides the length of the status messages
+$params['delimited'] = 'length'; // Provides the length of the status messages
 
 // ---------------------------------------------------------
 // Streaming API time limit and bounding box settings
@@ -24,8 +24,8 @@ $time_pre = microtime(true);
 $time_limit = 60;
 set_time_limit($time_limit + 30);
 
-// $boundingBox = $_POST["boundingBox"];
-// $params['locations'] = $boundingBox;
+
+// $params['locations'] = ...;
 
 // ------------------------------------------
 // Read the query string
@@ -40,19 +40,40 @@ $params['track'] = "mets";
 
 function my_streaming_callback($data, $length, $metrics) 
 {
-	// keep running time
+	// Keep running time
 	global $time_pre;
-	$time_post = microtime(true);
+	$time_post = microtime(true); // Returns the Unix timestamp with microseconds as a floating point value
 	$exec_time = $time_post - $time_pre;
 	global $time_limit;
-	if($exec_time > $time_limit){
+	if ( $exec_time > $time_limit )
+	{
 		return true;
 	}
-	$data = json_decode($data, true);
-	$date = strtotime($data['created_at']);
+
+	// BEGIN DEBUG: print the JSON before decoding to see all attribute value pairs
+	// echo $data;
+	// END DEBUG
+
+	// Create the file to which the data will be written
+	// TODO: make the filename dynamic (currently this needs to be changed for any run where we want a unique file)
+	// $outputFile = fopen("data_collection_output.csv", "w");
+	$outputFile = "data_collection_output.txt";
+
+	$data = json_decode($data, true); // Converts the JSON string to a PHP variable (and converts the output into an associative array)
 	$data['text'] = str_replace(PHP_EOL, '', $data['text']);
-	echo "{$data['id_str']}\t{$date}\t{$data['text']}" . PHP_EOL . "<br />";
-	flush();
+	// echo "{$data['id_str']}\t{$date}\t{$data['text']}" . PHP_EOL . "<br />";
+
+	$outputString = "id_str: " . "{$data['id_str']}" . " date created: " . "{$data['created_at']}" . "\n";
+	if (file_put_contents($outputFile, $outputString, FILE_APPEND) === FALSE)
+	{
+		// FALSE indicates that an error occurred during the fwrite operation
+	}
+	$outputString = "\tTweet text: " . "{$data['text']}" . "\n";
+	if (file_put_contents($outputFile, $outputString, FILE_APPEND) === FALSE)
+	{
+		// FALSE indicates that an error occurred during the fwrite operation
+	}
+
 	return file_exists(dirname(__FILE__) . '/STOP');
 }
 
