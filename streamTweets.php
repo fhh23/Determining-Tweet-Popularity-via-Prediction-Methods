ce979516b0a5bc65601c9ce7bd76bef98c009e2c
Bonnie R <bpr005@bucknell.edu>
2015-11-18 20:45:32 -0500

    Cut down features, renamed CSV headers, correctly formatted binary data. Pretty much the exact way I want this to look right now.

--- streamTweets.php
+++ streamTweets.php
@@ -68,9 +68,10 @@ function my_streaming_callback($data, $length, $metrics)
 	// Only prints the tweet information if $data has valid contents
 	if(!is_null($data['text']))
 	{
+		// Attempts to replace all newline characters in the tweets with an empty string
 		$newlineChars = array( PHP_EOL, '\n', '\r' );
 		// $data['text'] = str_replace(PHP_EOL, '', $data['text']);
-		$data['text'] = str_replace($newlineChars '', $data['text']);
+		$data['text'] = str_replace($newlineChars, '', $data['text']);
 		
 		// Create a data dump to have a log of all information 
 		// provided for each tweet
@@ -78,15 +79,11 @@ function my_streaming_callback($data, $length, $metrics)
 		// "php streamTwitter.php > [ data_dump_filename ]"
 		print_r($data); 
 		
-		/* Save any fields Array fields into separate variables for 
-		 * easier parsing */
-		
-		// Current Tweet data fields
+		// Save any fields Array fields into separate variables for 
+		// easier parsing
 		$entities = $data['entities'];
 		$place = $data['place'];
 		$user = $data['user'];
-		
-		// Retweeted status
 		$retweetedStatus = $data['retweeted_status'];
 		
 		/*
@@ -105,8 +102,8 @@ function my_streaming_callback($data, $length, $metrics)
 		if ($idStrCount > 98)
 		{
 			echo "Search API Tweet Limit Reached. Printing variable to file!";
-			$idStrString = "{$data['id_str']}" . "\n\n";
-			if (file_put_contents($searchAPIFile;, $idStrString, FILE_APPEND) === FALSE)
+			$idStrString = "{$idStrString}" . "," ."{$data['id_str']}" . "\n\n";
+			if (file_put_contents($searchAPIFile, $idStrString, FILE_APPEND) === FALSE)
 			{
 				// FALSE indicates that an error occurred during the fwrite operation
 			}
@@ -122,12 +119,12 @@ function my_streaming_callback($data, $length, $metrics)
 		else
 		{
 			$idStrCount = $idStrCount + 1;
-			$idStrString = "{$idStrString}" . "," "{$data['id_str']}";
+			$idStrString = "{$idStrString}" . "," . "{$data['id_str']}";
 		}
 		
 		
 		/* BEGIN FEATURE PRINTING */
-		$outputString = "{$data['id_str']}" . "," ."{$data['created_at']}" . "," "{$data['text']}" . "," . "{$data['truncated']}";
+		$outputString = "{$data['id_str']}" . "," . "{$data['created_at']}" . "," . "{$data['text']}" . ",";
 		
 		// Entities Array printing
 		$urlAndMediaCount = 0;
@@ -167,7 +164,6 @@ function my_streaming_callback($data, $length, $metrics)
 		}
 		$outputString = "{$outputString}" . "{$userMentionsCount}" . ",";
 		unset($userMentionsCount);
-		
 		if(is_array($entities['user_mentions']))
 		{
 			foreach($entities['user_mentions'] as $userMentions)
@@ -193,19 +189,118 @@ function my_streaming_callback($data, $length, $metrics)
 		$outputString = "{$outputString}" . "," . "{$hashtagCount}" . ",";
 		unset($hashtagCount);
 		
-		$outputString = "{$outputString}" . ($data['quoted_status_id_str'] <==> '') . ",";
+		if (strcmp($data['quoted_status_id_str'], '') == 0)
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
 		$outputString = "{$outputString}" . "{$data['quoted_status_id_str']}" . ",";
-		$outputString = "{$outputString}" . ($retweetedStatus['id_str'] <==> '') . ",";
+		if (strcmp($retweetedStatus['id_str'], '') == 0)
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
 		$outputString = "{$outputString}" . "{$retweetedStatus['id_str']}" . ",";
-		$outputString = "{$outputString}" . ($data['in_reply_to_screen_name'] <==> '') . ",";
-		$outputString = "{$outputString}" . "{$data['in_reply_to_screen_name']}" . ","
-		$outputString = "{$outputString}" . ($data['in_reply_to_status_id_str'] <==> '') . ",";
-		$outputString = "{$outputString}" . "{$data['in_reply_to_status_id_str']}" . ","
-		$outputString = "{$outputString}" . ($data['in_reply_to_user_id_str'] <==> '') . ",";
-		$outputString = "{$outputString}" . "{$data['in_reply_to_user_id_str']}" . ","
+		if (strcmp($data['in_reply_to_user_id_str'], '') == 0)
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		$outputString = "{$outputString}" . "{$data['in_reply_to_user_id_str']}" . ",";
+		if (strcmp($data['in_reply_to_screen_name'], '') == 0)
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		$outputString = "{$outputString}" . "{$data['in_reply_to_screen_name']}" . ",";
+		if (strcmp($data['in_reply_to_status_id_str'], '') == 0)
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		$outputString = "{$outputString}" . "{$data['in_reply_to_status_id_str']}" . ",";
 
-		$outputString = "{$outputString}" . "{$place['full_name']}" . "," . "{$data['possibly_sensitive']}" . ",";
-		$outputString = "{$outputString}" . "{$user['id_str']}" . "," . "{$user['name']}" . "," . "{$user['created_at']}" . "," . "{$user['statuses_count']}" . "," . "{$user['followers_count']}" . "," . "{$user['friends_count']}" . "," . "{$user['listed_count']}" . "," . "{$user['contributors_enabled']}" . "," . "{$user['geo_enabled']}" . "," . "{$user['protected']}" . "," . "{$user['verified']}" . "," . "{$user['default_profile']}" . "," . "{$user['default_profile_image']}";
+		$outputString = "{$outputString}" . "{$place['name']}" . ",";
+		
+		if ($data['possibly_sensitive'] == 1)
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		
+		$outputString = "{$outputString}" . "{$user['id_str']}" . "," . "{$user['name']}" . "," . "{$user['created_at']}" . "," . "{$user['statuses_count']}" . "," . "{$user['followers_count']}" . "," . "{$user['friends_count']}" . "," . "{$user['listed_count']}" . ",";
+		
+		if ($user['contributors_enabled'] == 1)
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		
+		if ($user['geo_enabled'] == 1)
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		
+		if ($user['protected']== 1)
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		
+		if($user['verified'] == 1)
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}	
+			
+		if ($user['default_profile'] == 1)
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
+		
+		if ($user['default_profile_image'] == 1)
+		{
+			$outputString = "{$outputString}" . "1" . ",";
+		}
+		else
+		{
+			$outputString = "{$outputString}" . "0" . ",";
+		}
 		/* END FEATURE PRINTING */
 		
 		if (file_put_contents($outputFile, $outputString, FILE_APPEND) === FALSE)
@@ -247,7 +342,7 @@ $tmhOAuth = new tmhOAuth($secretArray);
 // Create the file to which the data will be written
 $outputFile = "data_collection_output_" . date('Y-m-d-hisT') . ".csv";
 // print_r($outputFile);
-$searchAPIFile = "search_idStrings_" . date('Y-m-d-hisT') . "txt";
+$searchAPIFile = "search_idStrings_" . date('Y-m-d-hisT') . ".txt";
 // print_r($searchAPIFile);
 
 // Create the variables related to building for the Search API
@@ -255,15 +350,15 @@ $idStrCount = 0;
 $idStrString = '';
 
 // Print CSV file headers
-$dataHeaders = "coordinates,created_at,";
-$dataHeaders = "{$dataHeaders}" . "urls,user_mentions_id_str,user_mentions_names,hashtags,media,symbols,";
-$dataHeaders = "{$dataHeaders}" . "filter_level,id_str,in_reply_to_screen_name,in_reply_to_status_id_str,in_reply_to_user_id_str,place_id,place_type,place_name,place_country,possibly_sensitive,";
-$dataHeaders = "{$dataHeaders}" . "quoted_status_id_str,quoted_status_coordinates,quoted_status_creation_date,quoted_status_urls,quoted_status_user_mentions_id_str,quoted_status_user_mentions_name,quoted_status_user_mentions_screenname,quoted_status_hashtags,quoted_status_media,quoted_status_symbols,quoted_status_favorite_count,quoted_status_in_reply_to_screen_name,quoted_status_in_reply_to_status_id_str,quoted_status_in_reply_to_user_id_str,quoted_status_place_id,quoted_status_place_type,quoted_status_place_name,quoted_status_place_country,quoted_status_retweet_count,quoted_status_retweet_status_id_str,quted_status_retweet_text,quoted_status_retweet_favorite_count,quoted_status_retweet_retweet_count,quoted_status_source,quoted_status_text,quoted_status_user_id_str,quoted_status_user_name,quoted_status_user_screenname,quoted_status_user_location,quoted_status_user_creation_date,quoted_status_user_statuses_count,quoted_status_user_followers_count,quoted_status_user_following_count,quoted_status_user_listed_count,quoted_status_user_contributors_enabled,quoted_status_user_geo_enabled,quoted_status_protected_user,quoted_status_verified_user,quoted_status_default_user_profile,quoted_status_default_user_profile_image,quoted_status_user_withheld_in_countries,quoted_status_user_withheld_scope,";
-$dataHeaders = "{$dataHeaders}" . "scopes,";
-$dataHeaders = "{$dataHeaders}" . "retweeted_status_id_str,retweeted_status_coordinates,retweeted_status_creation_date,retweeted_status_urls,retweeted_status_user_mentions_id_str,retweeted_status_user_mentions_name,retweeted_status_user_mentions_screenname,retweeted_status_hashtags,retweeted_status_media,retweeted_status_symbols,retweeted_status_favorite_count,retweeted_status_in_reply_to_screen_name,retweeted_status_in_reply_to_status_id_str,retweeted_status_in_reply_to_user_id_str,retweeted_status_place_id,retweeted_status_place_type,retweeted_status_place_name,retweeted_status_place_country,retweeted_status_retweet_count,retweeted_status_quoted_status_id_str,retweeted_status_quoted_status_text,retweeted_status_quoted_status_favorite_count,retweeted_status_quoted_status_retweet_count,retweeted_status_source,retweeted_status_text,retweeted_status_user_id_str,retweeted_status_user_name,retweeted_status_user_screenname,retweeted_status_user_location,retweeted_status_user_creation_date,retweeted_status_user_statuses_count,retweeted_status_user_followers_count,retweeted_status_user_following_count,retweeted_status_user_listed_count,retweeted_status_user_contributors_enabled,retweeted_status_user_geo_enabled,retweeted_status_protected_user,retweeted_status_verified_user,retweeted_status_default_user_profile,retweeted_status_default_user_profile_image,retweeted_status_user_withheld_in_countries,retweeted_status_user_withheld_scope,";
-$dataHeaders = "{$dataHeaders}" . "source,text,truncated,";
-$dataHeaders = "{$dataHeaders}" . "user_id_str,user_name,user_screenname,user_location,user_creation_date,user_statuses_count,user_followers_count,user_following_count,user_listed_count,user_contributors_enabled,user_geo_enabled,protected_user,verified_user,default_user_profile,default_user_profile_image,user_withheld_in_countries,user_withheld_scope,";
-$dataHeaders = "{$dataHeaders}" . "tweet_withheld_copyright,tweet_withheld_in_countries,tweet_withheld_scope\n";
+$dataHeaders = "id_str,created_at,text,";
+$dataHeaders = "{$dataHeaders}" . "url_and_media_count,user_mentions_count,uer_mentions_names,hastags,hashtag_count,";
+$dataHeaders = "{$dataHeaders}" . "quoted_status,quoted_status_id_str,retweeted_status,retweeted_status_id_str,";
+$dataHeaders = "{$dataHeaders}" . "binary_in_reply_to_user_id,in_reply_to_user_id,";
+$dataHeaders = "{$dataHeaders}" . "binary_in_reply_to_screen_name,in_reply_to_screen_name,";
+$dataHeaders = "{$dataHeaders}" . "binary_in_reply_to_status_id,in_reply_to_status_id,";
+$dataHeaders = "{$dataHeaders}" . "place,possibly_sensitive,";
+$dataHeaders = "{$dataHeaders}" . "user_id_str,username,user_created_at,user_statuses_count,user_followers_count,user_following_count,user_list_count,user_contributors_enabled,user_geo_enabled,user_protected,user_verified,user_default_profile,user_default_profile_image\n";
+
 // Caution: overwrites any current data in the file
 if (file_put_contents($outputFile, $dataHeaders) === FALSE)
 {
